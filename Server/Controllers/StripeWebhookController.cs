5ecc402bd8c7f0a4122d002cfdcce12d65db21e3
TechKind <ken_hayakawa@tech-kind.biz>
2022-07-05 14:20:48 +0900

    支払い完了時のwebhook処理の追加

---
+++ Server/Controllers/StripeWebhookController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace BlazorECommerceApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class StripeWebhookController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public StripeWebhookController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("fulfill")]
+        public async ValueTask<IActionResult> FulFill()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(
+                    json,
+                    Request.Headers["Stripe-Signature"],
+                    _configuration["Stripe:WebhookSecret"]
+                );
+
+                if (stripeEvent.Type == Events.CheckoutSessionCompleted)
+                {
+                    var session = stripeEvent.Data.Object as Stripe.Checkout.Session;
+                }
+
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
